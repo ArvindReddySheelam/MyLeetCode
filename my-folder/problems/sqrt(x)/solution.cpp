fec7328316508ccb8afab1013198499ce0a905ae
Aravind Reddy <167006365+ArvindReddySheelam@users.noreply.github.com>
2024-04-05 06:59:13 +0000

    Sync LeetCode submission - Sqrt(x) (cpp)

---
+++ my-folder/problems/sqrt(x)/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 1 || x == 0){
+            return x;
+        }
+        int low = 1;
+        int high = x/2;
+        int res = 0;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if((long)mid * mid == x){
+                return mid;
+            }else if((long)mid * mid < x){
+                low = mid + 1;
+                res = mid;
+            }else{
+                high = mid - 1;
+            }
+        }
+
+        return res; 
+    }
+};
