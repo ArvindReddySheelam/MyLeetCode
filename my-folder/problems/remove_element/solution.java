004d583a285dd35371a1806d5b59e815866d0ca9
Aravind Reddy <167006365+ArvindReddySheelam@users.noreply.github.com>
2024-07-25 13:17:34 +0000

    Sync LeetCode submission - Remove Element (java)

--- my-folder/problems/remove_element/solution.java
+++ my-folder/problems/remove_element/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-   List<Integer> list = new ArrayList<>();
- int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[j] = nums[i];
-                   j++;
+        
+        int k = 0;
+
+        for(int i = 0; i<=nums.length-1; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
             }
         }
-        return j;
-       
-      
+
+        return k;
     }
 }
