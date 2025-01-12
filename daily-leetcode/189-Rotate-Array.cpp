4c474a6d2d04776528294367ab43878f577d3a2f
Aravind Reddy <167006365+ArvindReddySheelam@users.noreply.github.com>
2025-01-12 15:59:27 +0530

    [189] [Time Beats: 100.00%] [Memory Beats: 64.10%] - LeetPush

---
+++ daily-leetcode/189-Rotate-Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if(nums.size() == 0) return;
+        if(k == nums.size() || k == 0) return;
+
+        k %= nums.size();
+        
+        std::reverse(nums.begin(),nums.end());
+        std::reverse(nums.begin(),nums.begin()+k);
+        std::reverse(nums.begin()+k,nums.end());
+    }
+};
