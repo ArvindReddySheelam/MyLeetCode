74e95f13d4e5a1f83bf5b9c250e5e18fa0b1bd89
Aravind Reddy <167006365+ArvindReddySheelam@users.noreply.github.com>
2025-01-14 20:08:53 +0530

    [2657] [Time Beats: 81.21%] [Memory Beats: 67.17%] - LeetPush

---
+++ daily-leetcode/2657-Find-the-Prefix-Common-Array-of-Two-Arrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        
+        int n = A.size();
+        std::vector<int> freq(A.size() + 1);
+        int count = 0;
+
+        std::vector<int> C;
+
+        for(int i = 0; i<A.size(); i++){
+            freq[A[i]]++;
+            if(freq[A[i]] == 2) count++;
+
+            freq[B[i]]++;
+            if(freq[B[i]] == 2) count++;
+      
+            C.push_back(count);      
+        }
+
+        return C;
+    }
+};
